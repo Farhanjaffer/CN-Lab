5c1a8e0fd7a17ed424ddc9b62b0463a2dc9404ab
GraphicJaffer <farhanjaffer2002@gmail.com>
2024-02-08 16:34:52 +0530

    Add files via upload

---
+++ cn lab/lab1 24_07/new.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(int argc ,char *argv[])
+{
+    printf("program name %s\n", argv[0]);
+    printf("numbers right now:%s %s\n",argv[1],argv[2]);
+    //int temp= argc1;
+    //argc1=argc2;
+    //argc2=temp;
+}
---
+++ cn lab/lab4/Q2_Lab4.c
@@ -0,0 +1,1 @@
+//Command line argument se input a number uska next prime number print krega
